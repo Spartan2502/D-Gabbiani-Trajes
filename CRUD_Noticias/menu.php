a667ea5f6586e71dfb5ec8339a3b432e486539b8
Alejandro Orbe <josealexorbech@hotmail.com>
2022-02-14 15:29:25 -0600

    icons cruds

--- CRUD_Noticias/menu.php
+++ CRUD_Noticias/menu.php
@@ -6,6 +6,7 @@
 		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
 		<link rel="stylesheet" href="css/main.css">
 		<link rel="stylesheet" href="../css/section.css">
+		
 
 		<link rel="icon" href="img/D' Gabbiani.png">
 	</head>
@@ -43,18 +44,19 @@
         <div class="container-1" ">
             <div class="card" >
                 <div class="content">
-                    <div class="imgBx"><img src="img/product 1-test.png"></div>
+					
+                    <div class="imgBx"><img src="./img/traje-de-traje-y-corbata.png"></div>
                 </div>
                 <ul class="sci">
                     <li style="--i:1">
-                        <a href="./AltaRevistas.php" class="cta"><button>Administracion de rentas</button></a>
+                        <a href="./AltaRevistas.php" class="cta" ><button >Administracion de rentas</button></a>
                     </li>
                 </ul>
             </div>
             
             <div class="card" >
                 <div class="content">
-                    <div class="imgBx"><img src="img/product 2-test.png"></div>
+                    <div class="imgBx"><img src="./img/usuario.png"></div>
                 </div>
                 <ul class="sci">
                     <li style="--i:1">
